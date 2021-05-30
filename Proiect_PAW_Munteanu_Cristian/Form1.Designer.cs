2253399343a3a58a8fcb8c758a4b89dcab7186b7
Cristian Munteanu <munteanu.cristian1009@gmail.com>
2021-05-30 14:32:36 +0300

    interfata user

---
+++ Proiect_PAW_Munteanu_Cristian/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace Proiect_PAW_Munteanu_Cristian
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.guna2GradientCircleButton1 = new Guna.UI2.WinForms.Guna2GradientCircleButton();
+            this.guna2TextBoxUsername = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2TextBoxPassword = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2CircleButtonClose = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.comboBoxRol = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // guna2GradientCircleButton1
+            // 
+            this.guna2GradientCircleButton1.CheckedState.Parent = this.guna2GradientCircleButton1;
+            this.guna2GradientCircleButton1.CustomImages.Parent = this.guna2GradientCircleButton1;
+            this.guna2GradientCircleButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2GradientCircleButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2GradientCircleButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2GradientCircleButton1.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2GradientCircleButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2GradientCircleButton1.DisabledState.Parent = this.guna2GradientCircleButton1;
+            this.guna2GradientCircleButton1.FillColor = System.Drawing.Color.SkyBlue;
+            this.guna2GradientCircleButton1.FillColor2 = System.Drawing.Color.MediumSpringGreen;
+            this.guna2GradientCircleButton1.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2GradientCircleButton1.ForeColor = System.Drawing.Color.White;
+            this.guna2GradientCircleButton1.HoverState.Parent = this.guna2GradientCircleButton1;
+            this.guna2GradientCircleButton1.Location = new System.Drawing.Point(-185, -19);
+            this.guna2GradientCircleButton1.Name = "guna2GradientCircleButton1";
+            this.guna2GradientCircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2GradientCircleButton1.ShadowDecoration.Parent = this.guna2GradientCircleButton1;
+            this.guna2GradientCircleButton1.Size = new System.Drawing.Size(358, 573);
+            this.guna2GradientCircleButton1.TabIndex = 1;
+            // 
+            // guna2TextBoxUsername
+            // 
+            this.guna2TextBoxUsername.BorderColor = System.Drawing.Color.MediumSpringGreen;
+            this.guna2TextBoxUsername.BorderRadius = 15;
+            this.guna2TextBoxUsername.BorderThickness = 2;
+            this.guna2TextBoxUsername.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBoxUsername.DefaultText = "";
+            this.guna2TextBoxUsername.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBoxUsername.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBoxUsername.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBoxUsername.DisabledState.Parent = this.guna2TextBoxUsername;
+            this.guna2TextBoxUsername.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBoxUsername.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBoxUsername.FocusedState.Parent = this.guna2TextBoxUsername;
+            this.guna2TextBoxUsername.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2TextBoxUsername.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBoxUsername.HoverState.Parent = this.guna2TextBoxUsername;
+            this.guna2TextBoxUsername.IconLeft = ((System.Drawing.Image)(resources.GetObject("guna2TextBoxUsername.IconLeft")));
+            this.guna2TextBoxUsername.Location = new System.Drawing.Point(408, 212);
+            this.guna2TextBoxUsername.Name = "guna2TextBoxUsername";
+            this.guna2TextBoxUsername.PasswordChar = '\0';
+            this.guna2TextBoxUsername.PlaceholderText = "Username";
+            this.guna2TextBoxUsername.SelectedText = "";
+            this.guna2TextBoxUsername.ShadowDecoration.Parent = this.guna2TextBoxUsername;
+            this.guna2TextBoxUsername.Size = new System.Drawing.Size(229, 38);
+            this.guna2TextBoxUsername.TabIndex = 2;
+            // 
+            // guna2TextBoxPassword
+            // 
+            this.guna2TextBoxPassword.BorderColor = System.Drawing.Color.MediumSpringGreen;
+            this.guna2TextBoxPassword.BorderRadius = 15;
+            this.guna2TextBoxPassword.BorderThickness = 2;
+            this.guna2TextBoxPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBoxPassword.DefaultText = "";
+            this.guna2TextBoxPassword.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBoxPassword.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBoxPassword.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBoxPassword.DisabledState.Parent = this.guna2TextBoxPassword;
+            this.guna2TextBoxPassword.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBoxPassword.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBoxPassword.FocusedState.Parent = this.guna2TextBoxPassword;
+            this.guna2TextBoxPassword.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2TextBoxPassword.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBoxPassword.HoverState.Parent = this.guna2TextBoxPassword;
+            this.guna2TextBoxPassword.IconLeft = ((System.Drawing.Image)(resources.GetObject("guna2TextBoxPassword.IconLeft")));
+            this.guna2TextBoxPassword.Location = new System.Drawing.Point(408, 275);
+            this.guna2TextBoxPassword.Name = "guna2TextBoxPassword";
+            this.guna2TextBoxPassword.PasswordChar = '\0';
+            this.guna2TextBoxPassword.PlaceholderText = "Password";
+            this.guna2TextBoxPassword.SelectedText = "";
+            this.guna2TextBoxPassword.ShadowDecoration.Parent = this.guna2TextBoxPassword;
+            this.guna2TextBoxPassword.Size = new System.Drawing.Size(229, 38);
+            this.guna2TextBoxPassword.TabIndex = 3;
+            // 
+            // guna2CircleButtonClose
+            // 
+            this.guna2CircleButtonClose.BackColor = System.Drawing.Color.MintCream;
+            this.guna2CircleButtonClose.CheckedState.Parent = this.guna2CircleButtonClose;
+            this.guna2CircleButtonClose.CustomImages.Parent = this.guna2CircleButtonClose;
+            this.guna2CircleButtonClose.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButtonClose.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButtonClose.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2CircleButtonClose.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2CircleButtonClose.DisabledState.Parent = this.guna2CircleButtonClose;
+            this.guna2CircleButtonClose.FillColor = System.Drawing.Color.LightCoral;
+            this.guna2CircleButtonClose.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2CircleButtonClose.ForeColor = System.Drawing.Color.White;
+            this.guna2CircleButtonClose.HoverState.Parent = this.guna2CircleButtonClose;
+            this.guna2CircleButtonClose.Image = ((System.Drawing.Image)(resources.GetObject("guna2CircleButtonClose.Image")));
+            this.guna2CircleButtonClose.Location = new System.Drawing.Point(733, 12);
+            this.guna2CircleButtonClose.Name = "guna2CircleButtonClose";
+            this.guna2CircleButtonClose.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleButtonClose.ShadowDecoration.Parent = this.guna2CircleButtonClose;
+            this.guna2CircleButtonClose.Size = new System.Drawing.Size(44, 41);
+            this.guna2CircleButtonClose.TabIndex = 4;
+            // 
+            // comboBoxRol
+            // 
+            this.comboBoxRol.BackColor = System.Drawing.Color.White;
+            this.comboBoxRol.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBoxRol.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxRol.ForeColor = System.Drawing.Color.Black;
+            this.comboBoxRol.FormattingEnabled = true;
+            this.comboBoxRol.Items.AddRange(new object[] {
+            "ADMIN",
+            "USER"});
+            this.comboBoxRol.Location = new System.Drawing.Point(408, 163);
+            this.comboBoxRol.Name = "comboBoxRol";
+            this.comboBoxRol.Size = new System.Drawing.Size(229, 23);
+            this.comboBoxRol.TabIndex = 5;
+            this.comboBoxRol.Text = "Select Role";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(789, 530);
+            this.Controls.Add(this.comboBoxRol);
+            this.Controls.Add(this.guna2CircleButtonClose);
+            this.Controls.Add(this.guna2TextBoxPassword);
+            this.Controls.Add(this.guna2TextBoxUsername);
+            this.Controls.Add(this.guna2GradientCircleButton1);
+            this.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2GradientCircleButton guna2GradientCircleButton1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBoxUsername;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBoxPassword;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButtonClose;
+        private System.Windows.Forms.ComboBox comboBoxRol;
+    }
+}
+
